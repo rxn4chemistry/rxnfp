c37ded9eb47a6a174d25f804a775ee02384c9714
Philippe Schwaller <phs@zurich.ibm.com>
2020-09-02 19:45:49 +0200

    Fixed github url / use of hf save_vocab function / released version 0.0.5

--- rxnfp/tokenization.py
+++ rxnfp/tokenization.py
@@ -145,25 +145,6 @@ class SmilesTokenizer(BertTokenizer):
         else:
             return padding + token_ids
 
-    def save_vocabulary(self, vocab_path):
-        """Save the tokenizer vocabulary to a file."""
-        index = 0
-        vocab_file = vocab_path
-        with open(vocab_file, "w", encoding="utf-8") as writer:
-            for token, token_index in sorted(
-                self.vocab.items(), key=lambda kv: kv[1]
-            ):
-                if index != token_index:
-                    logger.warning(
-                        "Saving vocabulary to {}: vocabulary indices are not consecutive."
-                        " Please check that the vocabulary is not corrupted!".
-                        format(vocab_file)
-                    )
-                    index = token_index
-                writer.write(token + u"\n")
-                index += 1
-        return (vocab_file, )
-
 
 class BasicSmilesTokenizer(object):
     """Run basic SMILES tokenization"""
